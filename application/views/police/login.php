f68da84868b8414338261901d0f59fcc6792649b
arnav git <shah.arnav@yahoo.com>
2020-10-09 18:05:02 +0530

    changed investigation to management

--- application/views/police/login.php
+++ application/views/police/login.php
@@ -3,12 +3,12 @@
 		<div class="col-md-4 col-md-offset-4">
 			<h1 class="text-center"><?php echo $title; ?></h1>
 			<div class="form-group">
-				<input type="text" name="name" class="form-control" placeholder="Enter name" required autofocus>
+				<input type="text" autocomplete="off" name="name" class="form-control" placeholder="Enter name" required autofocus>
 			</div>
 			<div class="form-group">
-				<input type="password" name="password" class="form-control" placeholder="Enter Password" required autofocus>
+				<input type="password" autocomplete="off" name="password" class="form-control" placeholder="Enter Password" required autofocus>
 			</div>
-			<button type="submit" class="btn btn-primary btn-block">Login</button>
+			<button type="submit" autocomplete="off" class="btn btn-primary btn-block">Login</button>
 		</div>
 	</div>
 <?php echo form_close(); ?>
--- application/views/police/register.php
+++ application/views/police/register.php
@@ -6,35 +6,35 @@
 			<h1 class="text-center"><?= $title; ?></h1>
 			<div class="form-group">
 				<label>Name</label>
-				<input type="text" class="form-control" name="name" placeholder="Name">
+				<input type="text" autocomplete="off" class="form-control" name="name" placeholder="Name">
 			</div>
 			<div class="form-group">
 				<label>Age</label>
-				<input type="number"  min="18" class="form-control" name="age" placeholder="Age">
+				<input type="number" autocomplete="off" min="18" class="form-control" name="age" placeholder="Age">
 			</div>
 			<div class="form-group">
 				<label>Address</label>
-				<input type="text" class="form-control" name="address" placeholder="Address">
+				<input type="text" autocomplete="off" class="form-control" name="address" placeholder="Address">
 			</div>
 			<div class="form-group">
 				<label>Department</label>
-				<input type="text" class="form-control" name="department" placeholder="Department">
+				<input type="text" autocomplete="off" class="form-control" name="department" placeholder="Department">
 			</div>
 			<div class="form-group">
 				<label>Mobile Number</label>
-				<input type="tel" class="form-control" name="phone_number" placeholder="xxxx-xxx-xxx">
+				<input type="tel" autocomplete="off" class="form-control" name="phone_number" placeholder="xxxx-xxx-xxx">
 			</div>
 			<div class="form-group">
 				<label>Email</label>
-				<input type="email" class="form-control" name="email" placeholder="Email">
+				<input type="email" autocomplete="off" class="form-control" name="email" placeholder="Email">
 			</div>
 			<div class="form-group">
 				<label>Password</label>
-				<input type="password" class="form-control" name="password" placeholder="Password">
+				<input type="password" autocomplete="off" class="form-control" name="password" placeholder="Password">
 			</div>
 			<div class="form-group">
 				<label>Confirm Password</label>
-				<input type="password" class="form-control" name="password2" placeholder="Confirm Password">
+				<input type="password" autocomplete="off" class="form-control" name="password2" placeholder="Confirm Password">
 			</div>
 			<button type="submit" class="btn btn-primary btn-block">Submit</button>
 		</div>
